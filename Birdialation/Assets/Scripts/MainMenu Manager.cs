4303108f495b76eff8894b39cf75c177342ebdbc
profoundBoi <connor170103@gmail.com>
2024-11-15 13:42:09 +0200

    Added code that loads the different parts scenes for the levels

--- Birdialation/Assets/Scripts/MainMenu Manager.cs
+++ Birdialation/Assets/Scripts/MainMenu Manager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    public void Part01ButtonClicked()
     {
-        
+        SceneManager.LoadScene("Part01");
+    }
+    public void Part02ButtonClicked()
+    {
+        SceneManager.LoadScene("Part02");
+    }
+    public void Part03ButtonClickecd()
+    {
+        SceneManager.LoadScene("Part03");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
