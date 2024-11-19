cbffbbeb871e7f3d47c5093b793612f1920eca73
profoundBoi <connor170103@gmail.com>
2024-11-19 14:01:51 +0200

    stuff

--- Birdialation/Assets/Scripts/Button Manager.cs
+++ Birdialation/Assets/Scripts/Button Manager.cs
@@ -5,21 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+   
     public void Level1()
     {
         SceneManager.LoadScene("Level1");
     }
+
+ 
 
     public void Level2()
     {
         SceneManager.LoadScene("Level2");
 
     }
+  
 
     public void Level3()
     {
         SceneManager.LoadScene("Level3");
 
     }
+    
 }
 
--- Birdialation/Assets/Scripts/MainMenu Manager.cs
+++ Birdialation/Assets/Scripts/MainMenu Manager.cs
@@ -6,18 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-   
+    public GameObject Part01Button;
+    public GameObject Part02Button;
+    public GameObject Part03Button;
+
+    public GameObject Part01;
+    public GameObject Part02;
+    public GameObject Part03;
+
     public void Part01ButtonClicked()
     {
         SceneManager.LoadScene("Part01");
+    }
+    public void Next01()
+    {
+        Part01Button.SetActive(false);
+        Part02Button.SetActive(true);
+        Part01.SetActive(false);
+        Part02.SetActive(true);
     }
     public void Part02ButtonClicked()
     {
         SceneManager.LoadScene("Part02");
     }
+    public void Next02()
+    {
+        Part02Button.SetActive(false);
+        Part03Button.SetActive(true);
+        Part02.SetActive(false);
+        Part03.SetActive(true);
+    }
     public void Part03ButtonClickecd()
     {
         SceneManager.LoadScene("Part03");
+    }
+    public void Next03()
+    {
+        Part03Button.SetActive(false);
+        Part01Button.SetActive(true);
+        Part03.SetActive(false);
+        Part01.SetActive(true);
     }
 
 }
