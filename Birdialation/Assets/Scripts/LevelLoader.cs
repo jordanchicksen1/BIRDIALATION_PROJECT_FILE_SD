95d9ee3096dc628c500b4e262ea3c3e17f7bdefb
profoundBoi <connor170103@gmail.com>
2024-11-20 13:43:11 +0200

    added level scenes to build settings

---
+++ Birdialation/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void Level01ButtonClicked()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Level02ButtonClicked()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Level03ButtonClicked()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void Level04ButtonClicked()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+    public void Level05ButtonClicked()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void Level06ButtonClicked()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+    public void Level07ButtonClicked()
+    {
+        SceneManager.LoadScene("Level7");
+    }
+    public void Level08ButtonClicked()
+    {
+        SceneManager.LoadScene("Level8");
+    }
+    public void Level09ButtonClicked()
+    {
+        SceneManager.LoadScene("Level9");
+    }
+    public void Level10ButtonClicked()
+    {
+        SceneManager.LoadScene("Level10");
+    }
+    public void Level11ButtonClicked()
+    {
+        SceneManager.LoadScene("Level11");
+    }
+    public void Level12ButtonClicked()
+    {
+        SceneManager.LoadScene("Level12");
+    }
+    public void Level13ButtonClicked()
+    {
+        SceneManager.LoadScene("Level13");
+    }
+    public void Level14ButtonClicked()
+    {
+        SceneManager.LoadScene("Level14");
+    }
+    public void Level15ButtonClicked()
+    {
+        SceneManager.LoadScene("Level15");
+    }
+}
